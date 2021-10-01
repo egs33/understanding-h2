801402977af9b06529979a63def798fc2320499e
egs33 <egs33.bicycle@gmail.com>
2021-10-01 22:16:07 +0900

    Add TCP

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log(0);
+import { Tcp } from './protocol/tcp';
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
+  if (process.argv.length <= 2) {
+    console.error('argument required');
+    process.exit(1);
+  }
+  const url = process.argv[2];
+  const match = /^([a-z.]+):(\d+)$/.exec(url);
+  if (!match) {
+    console.error('argument parse error');
+    process.exit(1);
+  }
+  const hostname = match[1];
+  const port = Number.parseInt(match[2], 10);
+
+  const tcp = new Tcp(hostname, port, {
+    onData: async (data) => {
+      console.log(data.toString());
+      await tcp.end();
+      console.log('END');
+    },
+    onError: (error) => console.log('error', error),
+  });
+
+  await tcp.connect();
+  const resp = await tcp.write(`GET / HTTP/1.1\nHost:${hostname}\n\n`);
+  console.log(resp);
+})();
---
+++ src/protocol/tcp.ts
@@ -0,0 +1,64 @@
+import type { Socket } from 'net';
+import { createConnection } from 'net';
+
+export type TcpOption = Partial<{
+  encoding: BufferEncoding,
+  onClose: (hadError: boolean) => void,
+  onError: (error: Error) => void,
+  onData: (data: Buffer | string) => void,
+}>;
+
+export class Tcp {
+  private socket: Socket | null;
+
+  constructor(private hostname: string, private port: number, private option: TcpOption) {
+    this.socket = null;
+  }
+
+  public connect(): Promise<void> {
+    return new Promise(((resolve) => {
+      this.socket = createConnection(this.port, this.hostname, resolve);
+      if (this.option.encoding) {
+        this.socket.setEncoding(this.option.encoding);
+      }
+      if (this.option.onClose) {
+        this.socket.on('close', this.option.onClose);
+      }
+      if (this.option.onError) {
+        this.socket.on('error', this.option.onError);
+      }
+      if (this.option.onData) {
+        this.socket.on('data', this.option.onData);
+      }
+    }));
+  }
+
+  private checkWritable(): void {
+    if (!this.socket) {
+      throw new Error('Not connected');
+    }
+    if (!this.socket.writable) {
+      throw new Error('Not writable');
+    }
+  }
+
+  public write(
+    data: string | Buffer | Uint8Array,
+    encoding: BufferEncoding | undefined = undefined,
+  ): Promise<void> {
+    this.checkWritable();
+    return new Promise((resolve) => {
+      this.socket?.write(data, encoding, (error) => {
+        if (error) {
+          throw error;
+        }
+        resolve();
+      });
+    });
+  }
+
+  public end(): Promise<void> {
+    this.checkWritable();
+    return new Promise((resolve) => this.socket?.end(resolve));
+  }
+}
