0528812af7e9130d0fbbeb983adbc881c4d65d6c
egs33 <egs33.bicycle@gmail.com>
2022-02-02 00:15:42 +0900

    Refresh index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Tcp } from './protocol/tcp';
-import { Request } from './protocol/http1-1';
+/* eslint-disable */
+import { Tls1_2 } from './protocol/tls1-2';
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   if (process.argv.length <= 2) {
     console.error('argument required');
-    process.exit(1);
+    process.exit(1); // eslint-disable-line
   }
   const url = process.argv[2];
   const match = /^([a-z.]+):(\d+)$/.exec(url);
   if (!match) {
     console.error('argument parse error');
-    process.exit(1);
+    process.exit(1); // eslint-disable-line
   }
   const hostname = match[1];
   const port = Number.parseInt(match[2], 10);
 
-  const request = new Request(hostname, port, {});
-  const resp = await request.execute();
-  console.log(resp.getStatusCode());
-  console.log(resp.getStatusText());
-  console.log(resp.getHeaders());
+  const tls = new Tls1_2(hostname, port, {});
+  await tls.connect();
+  setTimeout(() => console.log(1000), 10000);
 })();
